997b8dd8b25bc61f8584609bc095be8c1b0469a7
Nick Trogh <ntrogh@hotmail.com>
2021-04-16 11:06:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http');
+
+// setup a web server
 http.createServer(function (req, res) {
   const options = {};
+  
+  // this should not pass code scanning - user input is not checked!
   send(req, req.url!, options);
+  
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.end();
